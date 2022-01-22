3c0997f113aa42e49c745df09c5024c23fdd1b3a
ssocolow <ssocolow@github.com>
2022-01-21 22:14:01 -0500

    added variable duration, clipping still a small issue, made max duration 5 secs

--- hashtone.js
+++ hashtone.js
@@ -1,3 +1,6 @@
+//max duration in milliseconds
+let DUR = 5000;
+
 //https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest
 async function digestMessage(message) {
   const msgUint8 = new TextEncoder().encode(message);                           // encode as (utf-8) Uint8Array
@@ -19,51 +22,59 @@ async function playSound(hex) {
   console.log(hex);
   document.getElementById("hsh").innerHTML = hex;
   wave = new p5.Oscillator();
+  //Options: 'sine' (default), 'triangle', 'sawtooth', 'square' (Optional)
   wave.setType('sine');
   wave.start();
-  for(let i = 0; i < hex.length; i++) {
-    changeSound(chooseSound(hex[i]),1);
-    await sleep(100);
+  //keep track of how much time music has been playing
+  let millistaken = 0;
+  for(let i = 0; i < hex.length; i+=2) {
+    if(millistaken <= DUR) { 
+      changeSound(chooseSound(hex[i]),0.1);
+      await sleep(getDuration(hex[i+1]));
+    }
+    millistaken += getDuration(hex[i+1]);
+    console.log(millistaken);
   }
-  wave.stop();
+  let fadetime = 0.2
+  wave.amp(0,fadetime);
+  await sleep(fadetime * 1000);
+  wave.stop(1);
 }
 
+//calculate the frequency in Hz of the key-th key on an idealized standard piano
+//https://en.wikipedia.org/wiki/Piano_key_frequencies
+function calcFrequency(key) {
+  return Math.pow(2, (key - 49)/12) * 440;
+}
+
+//for sound can vary: frequency, amplitude, duration
+
+//returns the base 10 number associated with hex char
+function getNum(hex) {
+  return parseInt(hex, 16);
+}
+
+//return the note's frequency from the hex character
+//key range: 37 - 67
+//range of frequency: 261.6 - 523.25 
 function chooseSound(c){
-  if(c === '0'){
-    return 440;
-  }if(c === '1'){
-    return 466;
-  }if(c === '2'){
-    return 493;
-  }if(c === '3'){
-    return 523;
-  }if(c === '4'){
-    return 554;
-  }if(c === '5'){
-    return 587;
-  }if(c === '6'){
-    return 622;
-  }if(c === '7'){
-    return 659;
-  }if(c === '8'){
-    return 698;
-  }if(c === '9'){
-    return 740;
-  }if(c === 'a'){
-    return 784;
-  }if(c === 'b'){
-    return 830;
-  }if(c === 'c'){
-    return 880;
-  }if(c === 'd'){
-    return 932;
-  }if(c === 'e'){
-    return 988;
-  }if(c === 'f'){
-    return 1046.5;
-  }else{
-    console.error(c + ' is not in hexadecimal format');
-  }
+  let num = getNum(c);
+  //num is 0 to 15
+  let key = num*2 + 37;
+  return calcFrequency(key); 
+}
+
+//map function
+function map(value, start1, stop1, start2, stop2) {
+  const newval = (value - start1) / (stop1 - start1) * (stop2 - start2) + start2;
+  return newval;
+}
+//return the note's duration from the next hex character
+//range of duration (milliseconds): 100 - 2000
+function getDuration(c) {
+  let n = getNum(c);
+  let duration = map(n, 0, 15, 50, 800);
+  return duration;
 }
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
