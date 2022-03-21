ddc30c372ef2cb9bc2a82f27598b0979908fceb9
ssocolow <ssocolow@github.com>
2022-03-20 22:40:41 -0400

    new calibration version, not working yet

--- tonemap/sketch.js
+++ tonemap/sketch.js
@@ -8,9 +8,8 @@ let wave;
 let minFreq = 2200;
 let maxFreq = 6840;
 
-//unused
-let diff = maxFreq - minFreq;
-let stepsize = diff/128;
+//the 8 frequencies that the sender will use, sufficiently far apart from each other
+let freqs = [2400, 2600, 2800, 3000, 3200, 3400, 3600, 3800];
 
 //first initialize the mic and fft but won't start working until user clicks the page
 function setup() {
@@ -35,6 +34,9 @@ function touchStarted() {
   listening = true;
 }
 
+//global variable to the bit string of the message
+let msg_bit_string;
+
 //when the user submits what they wrote
 function getInput(){
   //get the message
@@ -43,33 +45,58 @@ function getInput(){
   let encoder = new TextEncoder();
   let raw = encoder.encode(text);
   //convert the encoded string to music which will be played
-  playRaw(raw);
+  console.log(raw);
+  //convert the Uint8Array into one string on bits and add 
+  msg_bit_string = uint8tobits(raw);
+  //play the bit string
+  playRaw(msg_bit_string);
 }
 
+//takes an array of uint8s and returns a string of the all the bits concatenated
+function uint8tobits(raw){
+  let s = "";
+  for(let i = 0; i < raw.length; i++){
+    s += raw[i].toString(2);
+  }
+  return s;
+}
 //async so can play music with specific time intervals
-async function playRaw(raw) {
+async function playRaw(bitstring) {
   wave = new p5.Oscillator();
   //Options: 'sine' (default), 'triangle', 'sawtooth', 'square' (Optional)
   wave.setType('sine');
   wave.start();
 
   //starting preamble to get the listener tracking
-  changeSound(2164, 0.5);
-  await sleep(500);
-  //for each ASCII character, I play a specific sound for a constant time
-  for(let i = 0; i < raw.length; i++){
-    changeSound(chooseSound(raw[i]),0.5);
-    await sleep(300);
+  changeSound(2200, 1);
+  await sleep(1000);
+
+  //calibrate with the 8 info freqs
+  for(let i = 0; i < freqs.length; i++){
+    changeSound(freqs[i],1);
+    await sleep(100);
+  }
+  
+  //play the preamble right after to signify that those in-between frequencies are the info ones
+  changeSound(2200, 1);
+  await sleep(1000);
+
+
+  //send the bit string, 3 bits at a time
+  for(let i = 0; i < bitstring.length; i+=3){
+    changeSound(chooseSound(bitstring[i],bitstring[i+1],bitstring[i+2]),1);
+    await sleep(100);
   }
 
   //ending preamble to tell listener when done
-  changeSound(2164, 0.5);
-  await sleep(600);
+  changeSound(2200, 1);
+  await sleep(1000);
   wave.stop();
 }
 
 
 //async so can play music with specific time intervals
+//plays the preamble frequency for 2 seconds (2200 Hz) and then the 8 info frequencies in order from lowest to highest, 0.1 seconds on each, then preamble for one second
 async function preamble() {
   wave = new p5.Oscillator();
   //Options: 'sine' (default), 'triangle', 'sawtooth', 'square' (Optional)
@@ -77,16 +104,29 @@ async function preamble() {
   wave.start();
 
   //starting preamble to get the listener tracking
-  changeSound(2164, 0.5);
-  await sleep(10000);
+  changeSound(2200, 1);
+  await sleep(1000);
+
+  //the 8 info freqs
+  for(let i = 0; i < freqs.length; i++){
+    changeSound(freqs[i],1);
+    await sleep(100);
+  }
+  //play the preamble right after to signify that those in-between frequencies are the info ones
+  changeSound(2200, 1);
+  await sleep(1000);
+
   wave.stop();
 }
-//choose the frequency to play depending on which seven bit number (ASCII) given
-//input ranges from 32 to 126 (for ASCII: SPACE to ~)
-//output ranges from minFreq to maxFreq
-function chooseSound(byt){
-  let hz = map(byt, 32, 126, minFreq, maxFreq);
-  return hz;
+
+//choose the frequency that plays from the 3 bits from the string
+//will deal with endianness here by making the first bits most influential -- check here first if things aren't working, could have screwed this up pretty easily
+function chooseSound(bit1, bit2, bit3){
+  let index = 0;
+  index += parseInt(bit1,10)*4;
+  index += parseInt(bit2,10)*2;
+  index += parseInt(bit3,10)*1;
+  return freqs[index];
 }
 
 //simple change sound function
@@ -105,30 +145,22 @@ function sleep(ms) {
 let chars = [];
 //keep track of past buckets to filter out noise and only use sustained bucket
 let prev = [];
+//keep track of which bucket is the preamble
+let preamblebucket;
 
-
-//if there are ten buckets in the array, average them and if the rounded average is equal to 101 or 102 that will qualify as a preamble
-//preamble will be bucket number 102 (2156 Hz)
-//same with ending preamble
-//problem is when there is too many 100s and then higher value which averages to 102
+//check a bucket of 20 and if all are the same, assume it is the preamble
 function checkBucket() {
-  if(prev.length == 10){
-    let sum = 0;
-    let counthundreds = 0;
+  if(prev.length == 20){
+    let first_ = prev[0];
     for(let i = 0; i < prev.length; i++) {
-      sum += prev[i];
-      if(prev[i] == 100){
-        counthundreds++;
+      if(prev[i] != first_){
+        prev = [];
+        return false;
       }
     }
-    let avg = round(sum/prev.length);
-
-    if((102 == avg || 101 == avg) && counthundreds < 5){
-      startTime = millis();
-      return true;
-    }else{
-      prev = [];
-    }
+    preamblebucket = first_;
+    //console.log("hoi  " + preamblebucket);
+    return true;
   }
   return false;
 }
@@ -138,6 +170,39 @@ function checkBucket() {
 let listening = false;
 //if we are listening to the message
 let startTracking = false;
+
+//records all the buckets after the preamble
+let info = [];
+//collect the buckets where the info freqs go in
+let myinfobuckets = [];
+
+//fill myinfobuckets with an array of the buckets where the info freqs go - index 0 is preamblebucket
+//return the index in info off the first preamble after the info freqs
+function getInfoBuckets(){
+  let run = 0;
+  let recognized = [];
+  for(let i = 0; i < 100; i++){
+    if(Math.abs(info[i] - info[i+1]) < 5){
+      recognized.push(Math.round((info[i] + info[i+1])/2));
+    }else{
+      myinfobuckets.push(roundedAverage(recognized));
+      recognized = [];
+      if(myinfobuckets.length > 8){
+        return i+1;
+      }
+    }
+}
+}
+
+//return the rounded average of the list
+function roundedAverage(list){
+  let sum = 0;
+  for(let i = 0; i < list.length; i++){
+    sum += list[i];
+  }
+  return Math.round(sum/list.length);
+}
+
 
 //solving the start then stop before hearing the message problem: only check for ending preamble if more than 1 second after the beginning preamble
 let startTime = 0;
@@ -156,19 +221,23 @@ function draw() {
       startTracking = true;
       console.log("tracking");
       prev = [];
+      startTime = millis();
     }
   }
   
   if(startTracking){
-    //map the bucket number into the ASCII char range
-    let charnum = map(bucket, 104, 316, 32, 126);
-    //https://developer.mozilla.org/en-US/docs/Web/API/TextDecoder
-    let utf8decoder = new TextDecoder();
-    //decode the char number to a character and push it into the array
-    let u8arr = new Uint8Array([round(charnum)]);
-    chars.push(utf8decoder.decode(u8arr));
+    //record all the buckets after the preamble
+    info.push(bucket);
 
-    if(millis() - startTime > 1000){
+    //map the bucket number into the ASCII char range
+    //let charnum = map(bucket, 104, 316, 32, 126);
+    //https://developer.mozilla.org/en-US/docs/Web/API/TextDecoder
+    //let utf8decoder = new TextDecoder();
+    //decode the char number to a character and push it into the array
+    //let u8arr = new Uint8Array([round(charnum)]);
+    //chars.push(utf8decoder.decode(u8arr));
+
+    if(millis() - startTime > 3000){
       //when hearing ending preamble
       prev.push(bucket);
       if(checkBucket()){
@@ -176,8 +245,10 @@ function draw() {
         startTracking = false;
         listening = false;
         console.log("done listening");
-        console.log(chars);
-        let reconstructed = reConstruct(chars);
+        let leaveoffpoint = getInfoBuckets();
+        console.log("info buckets: " + myinfobuckets);
+        //now start at the leaveoffpoint and decode the message! - from buckets to bitstring to full message, can reuse the getInfoBuckets logic
+        let reconstructed = reConstruct(leaveoffpoint);
         document.getElementById("recieved").innerHTML = reconstructed;
       }
     }
@@ -197,37 +268,46 @@ function draw() {
   line(0, 140, width, 140);
  }
 
-//take in message array of chars and return the most common chars in order which will hopefully be the reconstructed message
-//if there are more than or equal to nine in a row then save that as one char
-function reConstruct(arr){
-  //save unique characters one for each time in a row
-  let uniques = [];
-  let uindex = -1;
-  let streakcount = [];
-  for(let i = 0; i < arr.length; i++) {
-    if(uniques[uindex] == arr[i]){
-      streakcount[uindex]++;
+//hold the decoded bit string
+let decodedbitstring;
+//hold the bucket parts of the message
+let messageparts = []
+//starts at the point in info where left off, then there is more preamble, then the message, then the preamble at the end
+function reConstruct(leaveoffpoint){
+  let run = 0;
+  let recognized = [];
+  for(let i = leaveoffpoint; i < info.length; i++){
+    if(Math.abs(info[i] - info[i+1]) < 5){
+      recognized.push(Math.round((info[i] + info[i+1])/2));
+    }else{
+      if(recognized.length > 2){
+        messageparts.push(roundedAverage(recognized));
+        recognized = [];
+      }
     }
-    else{
-      uindex++;
-      streakcount[uindex] = 0;
-      uniques.push(arr[i]);
+}
+  //go through messageparts and remove the elements that are not within 2 of a myinfobuckets and get to an array where all elements can be found in myinfobuckets
+  for(let i = messageparts.length - 1; i > 0; i--){
+    if(!myinfobuckets.includes(messageparts[i]) && !myinfobuckets.includes(messageparts[i] + 1) && !myinfobuckets.includes(messageparts[i] - 1) && !myinfobuckets.includes(messageparts[i] + 2) && !myinfobuckets.includes(messageparts[i] - 2)){ 
+      messageparts.splice(i,1);
     }
   }
-  let msg = "";
-  for(let i = 0; i < uniques.length; i++) {
-    if(streakcount[i] >= 9) {
-      msg += uniques[i];
+  for(let i = 0; i < messageparts.length; i++){
+    for(let j = 0; j < myinfobuckets.length; j++){
+      if(Math.abs(messageparts[i] - myinfobuckets[j]) <= 2){
+        messageparts[i] = myinfobuckets[j];
+      }
     }
   }
-  return msg;
+  console.log(messageparts);
+
 }
 
+
 //return the bucket with the frequency that is most prominent
-//buckets range from 104 (2200 Hz) to 316 (6840 Hz)
 function analyzeFrequency(spectrum) {
   let max = 0;
-  for(let i = 100; i < 320; i++){
+  for(let i = 0; i < 320; i++){
     if(spectrum[i] > max){
       max = i;
     }
